72c2eb98f59ffd7e3acfb53f97ed96784d0ef8fa
Ashay <clearkruti@gmail.com>
2017-12-24 20:52:57 +0530

    Start fresh

--- main_test.go
+++ main_test.go
@@ -1,38 +1,9 @@
 package main_test
 
 import (
-	"os"
 	"testing"
-
 	"github.com/ashayshub/tw-goodstuff"
 )
-
-func TestParseArgs(t *testing.T) {
-	u := main.UserProperty{Name: "ashay"}
-	u.ParseArgs()
-}
-
-func TestValidateArgs(t *testing.T) {
-	u := main.UserProperty{Name: "ashay"}
-	usage := u.ParseArgs()
-	if ok := u.ValidateArgs(&usage); !ok {
-		t.Fail()
-	}
-}
-
-func TestOAuth(t *testing.T) {
-	u := main.UserProperty{Name: "ashay"}
-	if _, ok := u.OAuth(); !ok {
-		t.Fail()
-	}
-}
-
-func BenchmarkOAuth(b *testing.B) {
-	u := main.UserProperty{Name: "ashay"}
-	for i := 0; i < b.N; i++ {
-		u.OAuth()
-	}
-}
 
 func TestMain(m *testing.M) {
 	os.Exit(m.Run())
