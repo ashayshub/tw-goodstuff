9a8e964173cf5e92b64fe38d5a56fcbd0eaeca41
Ashay <clearkruti@gmail.com>
2017-12-24 23:38:42 +0530

    Modified Test

--- main_test.go
+++ main_test.go
@@ -1,38 +1,12 @@
 package main_test
 
 import (
-	"fmt"
-	"net/http"
 	"os"
 	"testing"
+	// "net/http"
 
-	"github.com/ashayshub/tw-goodstuff"
+	// "github.com/ashayshub/tw-goodstuff"
 )
-
-const ep string = "http://" + main.HostAddr + ":" + main.HostPort
-
-func TestGetMostFavEp(t *testing.T){
-	epfv := ep + "/fav"
-	if _, er := http.Get(epfv); er != nil {
-		fmt.Println("Error: ", er)
-		t.Fail()
-	}
-}
-
-func TestGetMostRtEp(t *testing.T){
-	eprt := ep + "/rt"
-	if _, er := http.Get(eprt); er != nil {
-		fmt.Println("Error: ", er)
-		t.Fail()
-	}
-}
-
-func TestGetSlashEp(t *testing.T) {
-	if _, er := http.Get(ep); er != nil {
-		fmt.Println("Error: ", er)
-		t.Fail()
-	}
-}
 
 func TestMain(m *testing.M) {
 	os.Exit(m.Run())
