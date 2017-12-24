023ffca212341cb6ca8161f1dc1913490d171a32
Ashay <clearkruti@gmail.com>
2017-12-24 21:27:58 +0530

    Added Test case for checking the http endpoint on localhost

--- main_test.go
+++ main_test.go
@@ -1,9 +1,22 @@
 package main_test
 
 import (
+	"fmt"
+	"net/http"
 	"os"
 	"testing"
+
+	"github.com/ashayshub/tw-goodstuff"
 )
+
+func TestHTTPEndPoint(t *testing.T) {
+	ep := "http://" + main.HostAddr + ":" + main.HostPort
+	
+	if _, err := http.Get(ep); err != nil {
+		fmt.Println("Error: ", err)
+		t.Fail()
+	}
+}
 
 func TestMain(m *testing.M) {
 	os.Exit(m.Run())
