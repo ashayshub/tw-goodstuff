df413d286a0411eacfb094be311be7ca8af6ad23
Ashay <clearkruti@gmail.com>
2017-12-24 16:12:25 +0530

    Modified struct and their method to be importable from main_test.go

--- main.go
+++ main.go
@@ -6,45 +6,61 @@ import (
 	"log"
 	"os"
 	"strings"
+
+	"github.com/dghubble/go-twitter/twitter"
+	"golang.org/x/oauth2"
 )
 
 func main() {
-	user := new(userDetails)
-	user.parseArgs()
-	if ok := user.validateArgs(); !ok {
+
+	user := new(UserProperty)
+	usage := user.ParseArgs()
+	if ok := user.ValidateArgs(&usage); !ok {
 		log.Fatal("Validation failed")
 	}
-	fmt.Println("Your username is", strings.ToLower(user.name))
+	user.OAuth()
+	fmt.Println("Your username is", strings.ToLower(user.Name))
 }
 
-func usage() {
-	usageText := `Usage:
-%v: --username <username> -p[enter]
-<password>
-`
-	fmt.Printf(usageText, os.Args[0])
+type UserProperty struct {
+	Name        string
+	AccessToken string
 }
 
-type userDetails struct {
-	name     string
-	password string
-	token    string
-	login    bool
-}
+func (u *UserProperty) OAuth() (resp string, ok bool) {
+	config := &oauth2.Config{}
+	token := &oauth2.Token{AccessToken: u.AccessToken}
+	httpClient := config.Client(oauth2.NoContext, token)
 
-func (u *userDetails) oAuth() (resp string, ok bool) {
+	// Twitter client
+	client := twitter.NewClient(httpClient)
+
+	userShowParams := &twitter.UserShowParams{ScreenName: u.Name}
+	user, err, code := client.Users.Show(userShowParams)
+	if code != nil {
+		fmt.Println("Error: ", err)
+		return "", false
+	}
+	fmt.Printf("USERS SHOW:\n%+v, %#v, %#v\n", user, err, code)
 	return "", true
 }
 
-func (u *userDetails) parseArgs() {
-	flag.StringVar(&u.name, "username", "", "Enter your twitter username")
-	flag.Parse()
+func (u *UserProperty) ParseArgs() (f func()) {
+	flags := flag.NewFlagSet("tw-goodstuff", flag.ExitOnError)
+	flags.StringVar(&u.Name, "username", "", "Enter your twitter username")
+	flags.StringVar(&u.AccessToken, "app-access-token", "", "Twitter Application Access Token")
+	flags.Parse(os.Args[1:])
+	return flags.Usage
 }
 
-func (u *userDetails) validateArgs() (ok bool) {
-	if u.name == "" {
-		usage()
-		log.Print("--username flag cannot not be empty")
+func (u *UserProperty) ValidateArgs(usage *func()) (ok bool) {
+	if u.Name == "" {
+		(*usage)()
+		fmt.Println("Error: --username flag cannot not be empty")
+		return false
+	}
+	if u.AccessToken == "" {
+		fmt.Println("Error: --app-access-token flag cannot not be empty")
 		return false
 	}
 	return true
