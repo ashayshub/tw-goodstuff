cb29c571aa28e4b4eca4b8a9c1b1c986502a020b
Ashay <clearkruti@gmail.com>
2017-12-26 00:33:42 +0530

    Segregated tw_test.go from main_test.go

---
+++ twlib/tw_test.go
@@ -0,0 +1,32 @@
+package tw_test
+
+import (
+	"testing"
+	"net/http"
+	"net/http/httptest"
+	"github.com/ashayshub/tw-goodstuff/twlib"
+)
+
+func TestFavPage(t *testing.T) {
+	req := httptest.NewRequest(http.MethodGet, "http://locahost:8333/fav", nil)
+	_, status, ok := tw.FavPage(req)
+	if status != http.StatusOK || !ok {
+		t.Fail()
+	}
+}
+
+func TestRTPage(t *testing.T) {
+	req := httptest.NewRequest(http.MethodGet, "http://locahost:8333/rt", nil)
+	_, status, ok := tw.RTPage(req)
+	if status != http.StatusOK || !ok {
+		t.Fail()
+	}
+}
+
+func TestHomePage(t *testing.T) {
+	req := httptest.NewRequest(http.MethodGet, "http://locahost:8333/", nil)
+	_, status, ok := tw.HomePage(req)
+	if status != http.StatusOK || !ok {
+		t.Fail()
+	}
+}
