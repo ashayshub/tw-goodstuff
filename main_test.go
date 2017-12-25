02dbf608695ead3fca4e8f9e056476255b458df2
Ashay <clearkruti@gmail.com>
2017-12-25 23:19:13 +0530

    Removed for loop to for http.handle(), replaced with server struct

--- main_test.go
+++ main_test.go
@@ -15,12 +15,9 @@ func TestServeHTTP(t *testing.T) {
 	for _, route := range main.ActiveRoute {
 		req := httptest.NewRequest(http.MethodGet, main.EndPoint+route, nil)
 		resp := httptest.NewRecorder()
-
 		hd.ServeHTTP(resp, req)
-		resp.Flush()
-
-		fmt.Println(resp.Result())
 		if resp.Code != http.StatusOK {
+			fmt.Printf("%v: Status: %v, Body: %v, Result: %v\n", route, resp.Code,  resp.Body.String(), resp.Result())
 			t.Fail()
 		}
 	}
