f7928590b727ef792537219a560acb4f1fc5b2a5
Ashay <clearkruti@gmail.com>
2017-12-26 00:33:53 +0530

    Segregated tw_test.go from main_test.go

--- main_test.go
+++ main_test.go
@@ -23,30 +23,6 @@ func TestServeHTTP(t *testing.T) {
 	}
 }
 
-func TestFavPage(t *testing.T) {
-	req := httptest.NewRequest(http.MethodGet, main.EndPoint+"/fav", nil)
-	_, status, ok := main.FavPage(req)
-	if status != http.StatusOK || !ok {
-		t.Fail()
-	}
-}
-
-func TestRTPage(t *testing.T) {
-	req := httptest.NewRequest(http.MethodGet, main.EndPoint+"/rt", nil)
-	_, status, ok := main.RTPage(req)
-	if status != http.StatusOK || !ok {
-		t.Fail()
-	}
-}
-
-func TestHomePage(t *testing.T) {
-	req := httptest.NewRequest(http.MethodGet, main.EndPoint+"/rt", nil)
-	_, status, ok := main.RTPage(req)
-	if status != http.StatusOK || !ok {
-		t.Fail()
-	}
-}
-
 func TestMain(m *testing.M) {
 	os.Exit(m.Run())
 }
