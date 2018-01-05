a3b1361bbc23299028667f7195a25b546bcddf7d
Ashay <clearkruti@gmail.com>
2018-01-05 15:17:22 +0530

    Added a	struct twapp

---
+++ tw/tw_test.go
@@ -0,0 +1,36 @@
+package tw_test
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/ashayshub/tw-goodstuff/tw"
+)
+
+func TestFavPage(t *testing.T) {
+	a := tw.TwApp{}
+	req := httptest.NewRequest(http.MethodGet, "http://locahost:8333/fav", nil)
+	_, status, ok := a.FavPage(req)
+	if status != http.StatusOK || !ok {
+		t.Fail()
+	}
+}
+
+func TestRTPage(t *testing.T) {
+	a := tw.TwApp{}
+	req := httptest.NewRequest(http.MethodGet, "http://locahost:8333/rt", nil)
+	_, status, ok := a.RTPage(req)
+	if status != http.StatusOK || !ok {
+		t.Fail()
+	}
+}
+
+func TestHomePage(t *testing.T) {
+	a := tw.TwApp{}
+	req := httptest.NewRequest(http.MethodGet, "http://locahost:8333/", nil)
+	_, status, ok := a.HomePage(req)
+	if status != http.StatusOK || !ok {
+		t.Fail()
+	}
+}
